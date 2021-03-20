eade9d3b9fd01400dd263c5cc61cf529acd66d68
samuelov1 <jeanlouisfromtoge@gmail.com>
2021-03-20 17:06:31 +0200

    Add tests for updateMission route

--- server/test/routes/missions.test.js
+++ server/test/routes/missions.test.js
@@ -3,6 +3,7 @@ import chaiHttp from "chai-http";
 import mongoUnit from "mongo-unit";
 import * as utils from "../testUtils";
 import generateTestData from "../testData";
+import { ObjectId } from "mongodb";
 import { app } from "../../src/server";
 
 chai.use(chaiHttp);
@@ -24,6 +25,74 @@ describe("Missions route", () => {
           expect(err).to.be.null;
           expect(res.body).to.deep.have.same.members(expectedMissions);
           expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe("PUT: /missions", () => {
+    it("Should update mission", (done) => {
+      const originalMission = expectedMissions[0];
+      const missionToUpdate = {
+        _id: originalMission._id,
+        isCompleted: !originalMission.isCompleted,
+        content: "New Content",
+        createdAt: new Date(),
+        coordinates: {
+          lat: Math.random() * 100,
+          long: Math.random() * 100
+        }
+      };
+
+      const expected = utils.parseData(missionToUpdate);
+
+      chai
+        .request(app)
+        .put("/missions")
+        .send(missionToUpdate)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.body).to.deep.equal(expected);
+          expect(res).to.have.status(201);
+          done();
+        });
+    });
+
+    it("Should return notFoundError if no missions with given Id was found", (done) => {
+      const originalMission = expectedMissions[0];
+      const invalidMission = {
+        ...originalMission,
+        _id: new ObjectId()
+      };
+
+      chai
+        .request(app)
+        .put("/missions")
+        .send(invalidMission)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it("Should return ValidationError if invalid body is sent", (done) => {
+      const originalMission = expectedMissions[0];
+      const invalidMission = {
+        ...originalMission,
+        coordinates: {
+          lat: Math.random() * 100,
+          long: "invalid longtitude"
+        }
+      };
+
+      chai
+        .request(app)
+        .put("/missions")
+        .send(invalidMission)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(422);
           done();
         });
     });
