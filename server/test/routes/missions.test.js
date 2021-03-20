7998000d8408c4dbc4fe0cf6c26799767b388d2e
samuelov1 <jeanlouisfromtoge@gmail.com>
2021-03-20 17:09:00 +0200

    Add tests for insertMission route

--- server/test/routes/missions.test.js
+++ server/test/routes/missions.test.js
@@ -97,4 +97,82 @@ describe("Missions route", () => {
         });
     });
   });
+
+  describe("POST: /missions", () => {
+    it("Should insert mission", (done) => {
+      const missionToInsert = {
+        isCompleted: false,
+        content: "New Content",
+        createdAt: new Date(),
+        coordinates: {
+          lat: Math.random() * 100,
+          long: Math.random() * 100
+        }
+      };
+
+      const expected = utils.parseData(missionToInsert);
+
+      chai
+        .request(app)
+        .post("/missions")
+        .send(missionToInsert)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.body._id).to.exist;
+          expect(res.body.createdAt).to.exist;
+          expected._id = res.body._id;
+          expected.createdAt = res.body.createdAt;
+          expect(res.body).to.deep.equal(expected);
+          expect(res).to.have.status(201);
+          done();
+        });
+    });
+
+    it("Should set isCompleted: false, createdAt:[currentDate] as defaults if not given", (done) => {
+      const missionToInsert = {
+        content: "New Content",
+        coordinates: {
+          lat: Math.random() * 100,
+          long: Math.random() * 100
+        }
+      };
+
+      chai
+        .request(app)
+        .post("/missions")
+        .send(missionToInsert)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.body.isCompleted).to.be.false;
+
+          const createdAtTimestamp = new Date(res.body.createdAt).getTime();
+          const expectTimestamp = new Date().getTime();
+          const delta = 100000;
+          expect(createdAtTimestamp).to.be.closeTo(expectTimestamp, delta);
+
+          expect(res).to.have.status(201);
+          done();
+        });
+    });
+
+    it("Should return ValidationError if invalid body is sent", (done) => {
+      const invalidMission = {
+        content: "New Content",
+        coordinates: {
+          lat: Math.random() * 100,
+          long: "invalid longtitude"
+        }
+      };
+
+      chai
+        .request(app)
+        .post("/missions")
+        .send(invalidMission)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(422);
+          done();
+        });
+    });
+  });
 });
