76c94c6cf461add703227821d504acf190c66aa2
samuelov1 <jeanlouisfromtoge@gmail.com>
2021-03-29 11:01:29 +0300

    Disable filter button when no missions are selected

--- client/src/components/FilterButton.js
+++ client/src/components/FilterButton.js
@@ -1,27 +1,36 @@
+import { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IconButton, Tooltip } from "@material-ui/core";
 import { FilterList } from "@material-ui/icons";
-import { getShowCompletedMissions } from "../redux/selectors";
+import { getShowCompletedMissions, getAllMissions } from "../redux/selectors";
 import { setShowCompletedMissions } from "../redux/actions";
 
 const FilterButton = () => {
   const dispatch = useDispatch();
   const showCompletedMissions = useSelector(getShowCompletedMissions);
+  const missions = useSelector(getAllMissions);
+
+  const selectedMissions = useMemo(() => {
+    return missions.filter((mission) => mission.isCompleted);
+  }, [missions]);
 
   return (
     <Tooltip
       title={`${showCompletedMissions ? "Hide" : "Show"} Completed`}
       arrow
     >
-      <IconButton
-        color={showCompletedMissions ? "inherit" : "secondary"}
-        onClick={() =>
-          dispatch(setShowCompletedMissions(!showCompletedMissions))
-        }
-        aria-label="sort button"
-      >
-        <FilterList />
-      </IconButton>
+      <span>
+        <IconButton
+          color={showCompletedMissions ? "inherit" : "secondary"}
+          onClick={() =>
+            dispatch(setShowCompletedMissions(!showCompletedMissions))
+          }
+          aria-label="sort button"
+          disabled={!selectedMissions.length}
+        >
+          <FilterList />
+        </IconButton>
+      </span>
     </Tooltip>
   );
 };
